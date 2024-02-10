ea885a1791398b24d7cf5adadb225c7c626b360a
neon-nomad <matt.urrea.code@gmail.com>
2024-02-10 11:09:10 -0700

    wip: blocked by bitmap library issues

--- ShadowLink/ShadowLink/Services/ImageCipher.cs
+++ ShadowLink/ShadowLink/Services/ImageCipher.cs
@@ -1,105 +1,195 @@
+using System;
 using System.Drawing;
 using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
 
-namespace ShadowLink.Models;
-
-public class ImageCipher
+namespace ShadowLink.Services
 {
-      public static Bitmap? EmbedText(string text, Bitmap? bmp)
+    public class ImageCipher
     {
-        int pixelIndex = 0;
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
 
-        // for each character in the text
-        for (int i = 0; i < text.Length; i++)
+        public static Bitmap? EmbedText(string text, string seed, Bitmap? bmp)
         {
-            // for each bit in the character
-            for (int j = 0; j < 8; j++)
+            // Convert the seed to an integer
+            int seedInt = seed.GetHashCode();
+
+            // Use the seed to initialize the random number generator
+            Random rng = new Random(seedInt);
+            
+            // Encrypt the message before embedding
+            text = Encrypt(text, seed);
+
+            // Use a pseudo-random number generator to select pixels for embedding
+            int[] pixelIndices = Enumerable.Range(0, bmp.Width * bmp.Height).ToArray();
+            Shuffle(pixelIndices);
+
+            int pixelIndex = 0;
+
+            // for each character in the text
+            for (int i = 0; i < text.Length; i++)
             {
-                // get the pixel at the current index
-                Color pixel = bmp.GetPixel(pixelIndex % bmp.Width, pixelIndex / bmp.Width);
+                // for each bit in the character
+                for (int j = 0; j < 8; j += 2) // Use two bits per channel
+                {
+                    // get the pixel at the current index
+                    Color pixel = bmp.GetPixel(pixelIndices[pixelIndex] % bmp.Width, pixelIndices[pixelIndex] / bmp.Width);
 
-                // get the next bit in the text we want to hide
-                int bit = text[i] >> j & 1;
+                    // get the next two bits in the text we want to hide
+                    int bits = (text[i] >> j) & 3;
 
-                // change the least significant bit of each color channel
-                int r = pixel.R - pixel.R % 2 + bit;
-                int g = pixel.G - pixel.G % 2 + bit;
-                int b = pixel.B - pixel.B % 2 + bit;
+                    // change the two least significant bits of each color channel
+                    int r = pixel.R - pixel.R % 4 + bits;
+                    int g = pixel.G - pixel.G % 4 + bits;
+                    int b = pixel.B - pixel.B % 4 + bits;
 
-                // set the new pixel
-                bmp.SetPixel(pixelIndex % bmp.Width, pixelIndex / bmp.Width, Color.FromArgb(r, g, b));
+                    // set the new pixel
+                    bmp.SetPixel(pixelIndices[pixelIndex] % bmp.Width, pixelIndices[pixelIndex] / bmp.Width, Color.FromArgb(r, g, b));
 
-                pixelIndex++;
+                    pixelIndex++;
+                }
             }
+
+            return bmp;
         }
 
-        return bmp;
-    }
-
-    public static string ExtractText(Bitmap bmp)
-    {
-        int colorUnitIndex = 0;
-        int charValue = 0;
-
-        // holds the text that will be extracted from the image
-        StringBuilder extractedText = new StringBuilder();
-
-        // for each pixel in the image
-        for (int i = 0; i < bmp.Height; i++)
+        public static string ExtractText(string seed, Bitmap bmp)
         {
-            for (int j = 0; j < bmp.Width; j++)
+            // Convert the seed to an integer
+            int seedInt = seed.GetHashCode();
+
+            // Use the seed to initialize the random number generator
+            Random rng = new Random(seedInt);
+            
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            // holds the text that will be extracted from the image
+            StringBuilder extractedText = new StringBuilder();
+
+            // for each pixel in the image
+            for (int i = 0; i < bmp.Height; i++)
             {
-                Color pixel = bmp.GetPixel(j, i);
-
-                // for each color component (R, G, B)
-                for (int n = 0; n < 3; n++)
+                for (int j = 0; j < bmp.Width; j++)
                 {
-                    charValue = (colorUnitIndex % 3) switch
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    // for each color component (R, G, B)
+                    for (int n = 0; n < 3; n++)
                     {
-                        0 => charValue * 2 + pixel.R % 2,
-                        1 => charValue * 2 + pixel.G % 2,
-                        2 => charValue * 2 + pixel.B % 2,
-                        _ => charValue
-                    };
-
-                    colorUnitIndex++;
-
-                    // if 8 bits have been processed, add the current character to the result text
-                    if (colorUnitIndex % 8 == 0)
-                    {
-                        // reverse, since each time the process happens on the right 
-                        charValue = ReverseBits(charValue);
-
-                        // can only be 0 if it is the stop character (the 8 zeros)
-                        if (charValue == 0)
+                        charValue = (colorUnitIndex % 3) switch
                         {
-                            return extractedText.ToString();
+                            0 => charValue * 4 + pixel.R % 4,
+                            1 => charValue * 4 + pixel.G % 4,
+                            2 => charValue * 4 + pixel.B % 4,
+                            _ => charValue
+                        };
+
+                        colorUnitIndex++;
+
+                        // if 8 bits have been processed, add the current character to the result text
+                        if (colorUnitIndex % 4 == 0)
+                        {
+                            // reverse, since each time the process happens on the right 
+                            charValue = ReverseBits(charValue);
+
+                            // can only be 0 if it is the stop character (the 8 zeros)
+                            if (charValue == 0)
+                            {
+                                // Decrypt the extracted text
+                                return Decrypt(extractedText.ToString(), seed);
+                            }
+
+                            // convert the character value from int to char
+                            char c = (char)charValue;
+
+                            // add the current character to the result text
+                            extractedText.Append(c.ToString());
                         }
-
-                        // convert the character value from int to char
-                        char c = (char)charValue;
-
-                        // add the current character to the result text
-                        extractedText.Append(c.ToString());
                     }
                 }
             }
+
+            return extractedText.ToString();
         }
 
-        return extractedText.ToString();
-    }
-
-    public static int ReverseBits(int n)
-    {
-        int result = 0;
-
-        for (int i = 0; i < 8; i++)
+        public static int ReverseBits(int n)
         {
-            result = result * 2 + n % 2;
+            int result = 0;
 
-            n /= 2;
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
         }
 
-        return result;
-    }  
+        private static void Shuffle(int[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do rngCsp.GetBytes(box);
+                while (!(box[0] < n * (byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                int value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+        }
+
+        private static string Encrypt(string clearText, string key)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        private static string Decrypt(string cipherText, string key)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
 }
--- ShadowLink/ShadowLink/ViewModels/MainViewModel.cs
+++ ShadowLink/ShadowLink/ViewModels/MainViewModel.cs
@@ -1,92 +1,144 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Avalonia.Controls;
-using System.Drawing;
+using System.Collections.Generic;
+using ShadowLink.Services;
+using System.ComponentModel;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using ReactiveUI;
-using ShadowLink.Views;
-using ShadowLink.Models;
+using Bitmap = System.Drawing.Bitmap;
+
 
 namespace ShadowLink.ViewModels;
 
-public class MainViewModel : ViewModelBase
+public class MainViewModel : INotifyPropertyChanged
 {
-#pragma warning disable CA1822 // Mark members as static
-    public ICommand UploadImageCommand { get; set; }
-    public ICommand ExtractMessageCommand { get; set; }
-    public Window ParentWindow { get; }
+    private ImageCipher _imageCipher;
+    private string _message;
+    private string _seed;
+    private Bitmap _image;
+    private Avalonia.Media.Imaging.Bitmap _previewImage;
+    public ICommand UploadImageCommand { get; }
+    public ICommand EmbedTextCommand { get; }
+    public ICommand ExtractTextCommand { get; }
     
-    private Bitmap? _bitmap;
-    private string _userInput;
-
-    public string UserInputEmbedText
-    {
-        get =>
-            _userInput;
-        set =>
-            this.RaiseAndSetIfChanged(ref _userInput,
-                value);
-    }
-
-    public ICommand EmbedMessageCommand { get; }
-
     public MainViewModel()
     {
-        UploadImageCommand = ReactiveCommand.Create(UploadImage);
-        EmbedMessageCommand = ReactiveCommand.Create(EmbedMessage);
-        ExtractMessageCommand = ReactiveCommand.Create(ExtractMessage);
+        _imageCipher = new ImageCipher();
+        UploadImageCommand = ReactiveCommand.CreateFromTask(UploadImage);
+        EmbedTextCommand = ReactiveCommand.Create(EmbedText);
+        ExtractTextCommand = ReactiveCommand.Create(ExtractText);
     }
 
-    private async Task UploadImage()
+    public string Message
     {
-        var dialog = new OpenFileDialog();
-        dialog.Filters.Add(new FileDialogFilter()
+        get { return _message; }
+        set
         {
-            Name = "Image",
-            Extensions =
-            {
-                "jpg",
-                "png",
-                "bmp"
-            }
-        });
-
-        var result = await dialog.ShowAsync(this.ParentWindow);
-
-        if (result != null)
-        {
-            using (var stream = File.OpenRead(result[0])) // Open the file stream
-            {
-                _bitmap = new Bitmap(stream); // Store the image in the '_bitmap' variable
-            }
+            _message = value;
+            OnPropertyChanged();
         }
     }
 
+    public string Seed
+    {
+        get { return _seed; }
+        set
+        {
+            _seed = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Bitmap Image
+    {
+        get { return _image; }
+        set
+        {
+            _image = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Avalonia.Media.Imaging.Bitmap PreviewImage
+    {
+        get { return _previewImage; }
+        set
+        {
+            _previewImage = value;
+            OnPropertyChanged();
+        }
+    }
+
+    // TODO: Find a way to store Avalonia bitmap and System.Drawing bitmap in order to both operate and display the selected image. 
+
+    public async Task UploadImage()
+    {
+        var dialog = new OpenFileDialog
+        {
+            AllowMultiple = false,
+            Filters = new List<FileDialogFilter> { new FileDialogFilter { Name = "Images", Extensions = new List<string> { "png", "jpg", "jpeg", "bmp" } } }
+        };
+        
+        if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var result = await dialog.ShowAsync(desktop.MainWindow);
+
+            if (result != null && result.Length > 0)
+            {
+                using (var fs = new FileStream(result[0], FileMode.Open))
+                {
+                    var bitmap = new Bitmap(fs);
+                    Image = bitmap;
+                }
+                
+                using (var previewStream = new MemoryStream())
+                {
+                    Image.Save(previewStream, ImageFormat.Png);
+                    previewStream.Seek(0, SeekOrigin.Begin);
+
+                    PreviewImage = ConvertBitmapToAvalonia(new Bitmap(previewStream));
+                }
+            }
+        }
+        else
+        {
+            throw new Exception("Application is not a desktop application.");
+        }
+    }
     
-
-    public Bitmap? EmbedMessage()
+    private Avalonia.Media.Imaging.Bitmap ConvertBitmapToAvalonia(Bitmap bitmap)
     {
-
-        if (_bitmap != null && !string.IsNullOrEmpty(_userInput))
-        {
-            // Assuming ImageCipher is a static class and EmbedText is a static method
-            // Also assuming EmbedText takes a Bitmap and a string as parameters
-            return ImageCipher.EmbedText(_userInput,
-                _bitmap);
-        }
-        throw new Exception("No image uploaded or no message provided");
+        Console.WriteLine("POTATO");
+        return new Avalonia.Media.Imaging.Bitmap(Stream.Null);
+        // return (Avalonia.Media.Imaging.Bitmap)new ImageConverter().ConvertTo(bitmap, typeof(Avalonia.Media.Imaging.Bitmap));
+    }
+    
+    public void EmbedText()
+    {
+        // Image = _imageCipher.EmbedText(Message, Seed, Image);
+        Console.WriteLine("EmbedText");
     }
 
-
-    public void ExtractMessage()
+    public void ExtractText()
     {
-        Debug.WriteLine("extract clicked");
-        // Get the bitmap from memory
-        // Call the ExtractText method from the ImageCipher.cs file
-        // Display the extracted message in the text box
+        // Message = _imageCipher.ExtractText(Seed, Image);
+        Console.WriteLine("ExtractText");
     }
 
-#pragma warning restore CA1822 // Mark members as static
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
